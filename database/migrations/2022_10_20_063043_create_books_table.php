dd7ce3906ce2c81a6a26d823e83e6578cb892033
sabras <subairsabras4565@gmail.com>
2022-10-24 22:42:25 +0530

    my project

---
+++ database/migrations/2022_10_20_063043_create_books_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('author_id');
+            $table->string('Book Id', 100);
+            $table->unsignedInteger('category_id');
+            $table->string('Title', 100);
+            $table->string('Description', 100);
+            $table->string('Genre/type', 100);
+            $table->string('price', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+};
